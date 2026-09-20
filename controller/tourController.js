dbd8385cd1aea7fd0470c23d0635a2a8222b860c
Nimesh <lov.nimesh2024@lpu.in>
2026-09-20 21:52:17 +0530

    implemented pagination and routeAlias

--- controller/tourController.js
+++ controller/tourController.js
@@ -2,48 +2,82 @@ import Tour from '../models/tourModel.js';
 
 // 2) ROUTE HANDLERS
 
-const getAllTours = async (req, res) => {
-  try {
-    // BUILD QUERY
-    // 1A) Filtering
-    const queryObj = { ...req.query };
+const aliasTopTours = (req, _res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,pric e,ratingAverage,difficutly';
+
+  next();
+};
+
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
     const excludedFields = ['page', 'sort', 'fields', 'limit'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
     // 1B) Advance Filtering
 
-    // {difficulty: 'easy', duration: {gte: 5}}
-
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    let query = Tour.find(JSON.parse(queryStr));
+    this.query = this.query.find(JSON.parse(queryStr));
 
-    // 2) SORTING
-    if (req.query.sort) {
-      query = query.sort(req.query.sort.replace(',', ' '));
+    return this;
+  }
+
+  // sorting function
+  sort() {
+    if (this.queryString.sort) {
+      this.query = this.query.sort(this.queryString.sort.replaceAll(',', ' '));
       // sort('price ratingAverage')
     } else {
-      query = query.sort('-createdAt');
+      this.query = this.query.sort('-createdAt');
     }
 
-    // 3) FIELD LIMITING
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
+    return this;
+  }
+
+  // 3) FIELD LIMITING
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
     } else {
-      query = query.select('-__v');
-      // -prefix is used to exclude the field
+      this.query = this.query.select('-__v');
     }
 
-    // const query = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
+    return this;
+  }
 
+  // 4) PAGINATION
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = limit * (page - 1);
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
+const getAllTours = async (req, res) => {
+  try {
     // EXECUTE QUERY
-    const tours = await query;
+    // Here we are able to chain it, because we are returning the object in each method
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    const tours = await features.query;
 
     res.status(200).json({
       status: 'successfull',
@@ -72,7 +106,7 @@ const getTour = async (req, res) => {
       },
     });
   } catch (err) {
-    res.stauts(404).json({
+    res.status(404).json({
       status: 'failed',
       message: err,
     });
@@ -142,4 +176,11 @@ const deleteTour = async (req, res) => {
   }
 };
 
-export { getAllTours, getTour, createTour, updateTour, deleteTour };
+export {
+  getAllTours,
+  getTour,
+  createTour,
+  updateTour,
+  deleteTour,
+  aliasTopTours,
+};
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,11 +6,15 @@ import {
   updateTour,
   deleteTour,
   createTour,
+  aliasTopTours,
 } from '../controller/tourController.js';
 
 const router = express.Router();
 
 // router.param('id', checkID);
+
+// route alias for top tours
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 // if id is not valid then it will return from here no need to verfiy in all the request handler
 router.route('/').get(getAllTours).post(createTour);
