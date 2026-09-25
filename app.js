c47af316d5015c760c200736109b02e036bb62ee
Nimesh <lov.nimesh2024@lpu.in>
2026-09-25 21:48:31 +0530

    Error handling in Express

--- app.js
+++ app.js
@@ -26,12 +26,6 @@ app.use(express.static(`${__dirname}/public`));
 // to define our own middleware we use 'app.use()' function
 
 // middleware function is going to execute at every kind of request
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-
-  next();
-  // next is use to call the next middleware
-});
 
 // manuplating the req;
 app.use((req, res, next) => {
@@ -47,5 +41,30 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// .all() method handles the request coming from any http method
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find the ${req.originalUrl} on this server.`,
+  });
+});
+
+// GLOBAL ERROR HANDLING MIDDLEWARE
+
+// Express comes with middleware handlers
+
+app.use((err, req, res, next) => {
+  // by specifying the 4 args above, express recognizes it as a error handling middleware
+
+  // defining the statusCode and stuatus for error object when it is undefined
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 export default app;
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import slugify from 'slugify';
+import validator from 'validator';
 
 const tourSchema = mongoose.Schema(
   {
